88344fc6b8d83ec6b254b5dc55ef6cc28fca8e04
Dave-Romano <dave@online.ua>
2022-06-03 23:26:09 +0300

    lead-upV1

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,41 @@
 import { createArray } from "../helpers/index";
 
+const stateProto = {
+  counter: {
+    minutes: 0,
+    hours: 0,
+    day: {
+      number: 1,
+      name: "monday",
+    },
+    month: {
+      number: 1,
+      name: "september",
+    },
+    year: 0,
+  },
+  enemies: [],
+
+  players: [],
+  trade: {},
+  items: {
+    weapons: {
+      items: [
+        {
+          name: "sword",
+          description: "low level common sword",
+          rarity: 1,
+          weight: 1,
+          phisicalDmg: 1,
+          magicDmg: 1,
+          price: 1,
+          effects: {},
+        },
+      ],
+    },
+  },
+};
+
 export const allmonths = [
   "Зори (январь)",
   "Солнца (февраль)",
